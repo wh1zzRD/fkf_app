2771d6076593e639db0044752433067401d679f5
Tim Seleznev <seleznev_tim@outlook.com>
2024-05-23 19:37:16 +0200

    Fix documentation and workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+sys.setrecursionlimit(1500)
+
 project = 'FKF App'
 copyright = '2024, Tim Seleznev'
 author = 'Tim Seleznev'
@@ -13,15 +18,27 @@ author = 'Tim Seleznev'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+latex_elements = {
+    'papersize': 'letterpaper',
+    'pointsize': '10pt',
+    'preamble': '',
+    'figure_align': 'htb'
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
+
+source_encoding = 'utf-8'
